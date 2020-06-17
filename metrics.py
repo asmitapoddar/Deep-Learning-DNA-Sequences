4dd0dc4c1acac4159150b07a26ee391b7022e744
asmitapoddar <asmita.poddar@cs.ox.ac.uk>
2020-06-17 23:01:17 +0100

    eval metrics

---
+++ metrics.py
@@ -0,0 +1,17 @@
+import torch
+import numpy as np
+from sklearn import metrics
+
+def metrics_from_raw(raw, y_true):
+    '''
+    Function to return evaluation metrics for regression
+    :param raw: raw prediction obtained from forward pass of model
+    :param y_true: actual value
+    :return: Mean Absolute Error, Mean Square Error, R2 Score
+    '''
+    y_pred = raw.numpy()
+    mae = metrics.mean_absolute_error(y_true.numpy(), y_pred)
+    mse = metrics.mean_squared_error(y_true.numpy(), y_pred)
+    r2_score = metrics.r2_score(y_true.numpy(), y_pred)
+    return mae, mse, r2_score
+
