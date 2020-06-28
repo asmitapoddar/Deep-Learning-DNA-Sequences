b0d1f8b48445a62c735ec00bc675f314a7d367e6
asmitapoddar <asmita.poddar@cs.ox.ac.uk>
2020-06-28 01:30:47 +0100

    metrics for classification+regression

--- Code/metrics.py
+++ Code/metrics.py
@@ -15,3 +15,20 @@ def metrics_from_raw(raw, y_true):
     r2_score = metrics.r2_score(y_true.numpy(), y_pred)
     return mae, mse, r2_score
 
+def pred_from_raw(raw):
+    pred = torch.argmax(torch.softmax(raw, dim=1), dim=1).cpu().numpy()
+    return pred
+
+
+def metrics_classification(raw, y_true, avg):
+    pred = pred_from_raw(raw)
+    print(pred)
+    f1 = metrics.f1_score(pred, y_true.numpy(), average=avg, zero_division=0)
+    prec = metrics.precision_score(pred, y_true.numpy(), average=avg, zero_division=0)
+    rec = metrics.recall_score(pred, y_true.numpy(), average=avg, zero_division=0)
+    return f1, prec, rec
+
+
+def accuracy_from_raw(raw, y_true):
+    acc =  metrics.accuracy_score(pred_from_raw(raw), y_true.numpy())
+    return acc
