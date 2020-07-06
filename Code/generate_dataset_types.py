9668535fa0fcf6373ecf617320bae23d93c1a530
asmitapoddar <asmita.poddar@cs.ox.ac.uk>
2020-07-06 23:54:10 +0100

    creating diff types of  datasets

---
+++ Code/generate_dataset_types.py
@@ -0,0 +1,89 @@
+import pandas as pd
+from dataset_utils import *
+
+columns = ['Type', 'Sequence', 'Interval_Indices', 'GeneID', 'Gene_Strand', 'Boundary_Position']
+global finaldf
+finaldf = pd.DataFrame(columns=columns)  # For sanity check
+
+def boundary_exon_intron_3classification(exon_boundary_set_final,exon_boundary_final,
+                                         within_exon_seq_intervals, within_intron_seq_intervals,
+                                         gene, MAX_LENGTH, OFFSET_RANGE):
+    '''
+        Function to create training set [start intervals/ end intervals] (positive samples)
+        :param exon_boundary_intervals_final: list of intervals
+        :param exon_boundary_final: list of int containing the exon boundaries
+        :param within_exon_seq_intervals: list of intervals
+        :param within_intron_seq_intervals: list of intervals
+        :param gene: gene information (JSON)
+        :param MAX_LENGTH: int
+        :return: 2 lists
+                list of str: training_set_x containing the DNA seqeunces
+                list of int: training_set_y containing the exon boundary position
+
+        Sequences containing Exon Boundary: Class 0
+                            Purely Exonic Sequences: Class 1
+                            Purely Intronic Sequences: Class 2
+        '''
+    sxboundary, syboundary = create_training_set(exon_boundary_set_final,
+                                                 [0] * len(exon_boundary_final), gene, MAX_LENGTH)
+    sxexon, syexon = create_training_set(within_exon_seq_intervals,
+                                         [1] * len(within_exon_seq_intervals), gene, MAX_LENGTH)
+    sxintron, syintron = create_training_set(within_intron_seq_intervals,
+                                             [2] * len(within_intron_seq_intervals), gene, MAX_LENGTH)
+
+    dfboundary = sanity_check(sxboundary, exon_boundary_set_final, columns, 'Boundary', gene, exon_boundary_final)
+    # optionally send these dependi ng on the dataset type
+    dfexon = sanity_check(sxexon, within_exon_seq_intervals, columns, 'Exon', gene)
+    dfintron = sanity_check(sxintron, within_intron_seq_intervals, columns, 'Intron', gene)
+    finaldataframe = finaldf.append([dfboundary, dfexon, dfintron])
+    return (sxboundary + sxexon + sxintron, syboundary + syexon + syintron)
+
+def boundary_orNot_2classification(exon_boundary_set_final,exon_boundary_final,
+                                         within_exon_seq_intervals, within_intron_seq_intervals,
+                                         gene, MAX_LENGTH, OFFSET_RANGE):
+    sxboundary, syboundary = create_training_set(exon_boundary_set_final,
+                                                 [0] * len(exon_boundary_final), gene, MAX_LENGTH)
+    sxexon, syexon = create_training_set(within_exon_seq_intervals,
+                                         [1] * len(within_exon_seq_intervals), gene, MAX_LENGTH)
+    sxintron, syintron = create_training_set(within_intron_seq_intervals,
+                                             [1] * len(within_intron_seq_intervals), gene, MAX_LENGTH)
+    return (sxboundary + sxexon + sxintron, syboundary + syexon + syintron)
+
+def find_boundary_Nclassification(exon_boundary_set_final, exon_boundary_final,
+                                         within_exon_seq_intervals, within_intron_seq_intervals,
+                                         gene, MAX_LENGTH, OFFSET_RANGE):
+    ''' todo need to get offset
+    Sequences containing Exon Boundary: Boundary point
+    Purely Exonic Sequences: 0
+    Purely Intronic Sequences: 0
+    '''
+    sxboundary, syboundary = create_training_set(exon_boundary_set_final,
+                                                 [x - OFFSET_RANGE[0] for x in exon_boundary_final], gene, MAX_LENGTH)
+    return (sxboundary, syboundary)
+
+def boundaryCertainPoint_orNot_2classification(exon_boundary_set_final, exon_boundary_final,
+                                         within_exon_seq_intervals, within_intron_seq_intervals,
+                                         gene, MAX_LENGTH, OFFSET_RANGE):
+    #TODO: TEST IT -------
+    sxboundary, syboundary = create_training_set(exon_boundary_set_final,
+                                                 [0] * len(exon_boundary_final), gene, MAX_LENGTH)
+    sxexon, syexon = create_training_set(within_exon_seq_intervals,
+                                         [1] * len(within_exon_seq_intervals), gene, MAX_LENGTH)
+    sxintron, syintron = create_training_set(within_intron_seq_intervals,
+                                             [1] * len(within_intron_seq_intervals), gene, MAX_LENGTH)
+    return (sxboundary + sxexon + sxintron, syboundary + syexon + syintron)
+
+def seq_1classification(exon_boundary_set_final, exon_boundary_final,
+                                         within_exon_seq_intervals, within_intron_seq_intervals,
+                                         gene, MAX_LENGTH, OFFSET_RANGE):
+    sxboundary, syboundary = create_training_set(exon_boundary_set_final,
+                                                 [0] * len(exon_boundary_final), gene, MAX_LENGTH)
+    return (sxboundary, syboundary)
+
+def find_boundary_regression(exon_boundary_set_final, exon_boundary_final,
+                                         within_exon_seq_intervals, within_intron_seq_intervals,
+                                         gene, MAX_LENGTH, OFFSET_RANGE):
+    sxboundary, syboundary = create_training_set(
+        exon_boundary_set_final, exon_boundary_final, gene, MAX_LENGTH)
+    return (sxboundary, syboundary)
+
