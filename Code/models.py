6d20ec139a44151dc86b7f0980f6f6789e8e85c3
asmitapoddar <asmita.poddar@cs.ox.ac.uk>
2020-07-14 23:38:24 +0100

    add attention model

--- Code/models.py
+++ Code/models.py
@@ -16,6 +16,8 @@ class BaseModel(nn.Module):
         params = sum([np.prod(p.size()) for p in model_parameters])
         return super().__str__() + '\nTrainable parameters: {}'.format(params)
 
+
+''' ******** Simple Artificial Neural Network ***********'''
 class BasicNN(nn.Module):
 
     def __init__(self, inp, h, d, out):
@@ -40,7 +42,7 @@ class BasicNN(nn.Module):
 
         return X
 
-
+''' ***** Simple LSTM ***** '''
 class SimpleLSTM(BaseModel):
 
     def __init__(self, input_dims, hidden_units, hidden_layers, out, batch_size, device):
@@ -74,3 +76,93 @@ class SimpleLSTM(BaseModel):
         lstm_out, (h_n, c_n) = self.lstm(input, hidden)
         raw_out = self.output_layer(h_n[-1])
         return raw_out
+
+''' ***** attention Model ***** '''
+
+def batch_product(iput, mat2):
+    result = None
+    for i in range(iput.size()[0]):
+        op = torch.mm(iput[i], mat2)
+        op = op.unsqueeze(0)
+        if (result is None):
+            result = op
+        else:
+            result = torch.cat((result, op), 0)
+    return result.squeeze(2)
+
+
+class rec_attention(nn.Module):
+    # attention with bin context vector per HM and HM context vector
+    def __init__(self, hm, args):
+        super(rec_attention, self).__init__()
+        self.num_directions = 2 if args.bidirectional else 1
+        if (hm == False):
+            self.bin_rep_size = args.bin_rnn_size * self.num_directions
+        else:
+            self.bin_rep_size = args.bin_rnn_size
+
+        self.bin_context_vector = nn.Parameter(torch.Tensor(self.bin_rep_size, 1), requires_grad=True)
+        self.softmax = nn.Softmax(dim=1)
+        self.bin_context_vector.data.uniform_(-0.1, 0.1)
+
+    def forward(self, iput):
+        alpha = self.softmax(batch_product(iput, self.bin_context_vector))
+        [source_length, batch_size, bin_rep_size2] = iput.size()
+        repres = torch.bmm(alpha.unsqueeze(2).view(batch_size, -1, source_length),
+                           iput.reshape(batch_size,source_length,bin_rep_size2))
+        return repres, alpha
+
+
+class recurrent_encoder(nn.Module):
+    # modular LSTM encoder
+    def __init__(self, n_bins, ip_bin_size, hm, args):
+        super(recurrent_encoder, self).__init__()
+        self.bin_rnn_size = args.bin_rnn_size
+        self.ipsize = ip_bin_size
+        self.seq_length = n_bins
+
+        self.num_directions = 2 if args.bidirectional else 1
+        if (hm == False):
+            self.bin_rnn_size = args.bin_rnn_size
+        else:
+            self.bin_rnn_size = args.bin_rnn_size // 2
+        self.bin_rep_size = self.bin_rnn_size * self.num_directions
+
+        self.rnn = nn.LSTM(self.ipsize, self.bin_rnn_size, num_layers=args.num_layers, dropout=args.dropout,
+                           bidirectional=args.bidirectional)
+        self.bin_attention = rec_attention(hm, args)
+
+    def outputlength(self):
+        return self.bin_rep_size
+
+    def forward(self, single_hm, hidden=None):
+        bin_output, hidden = self.rnn(single_hm, hidden)
+        bin_output = bin_output.permute(1, 0, 2)
+        nt_rep, bin_alpha = self.bin_attention(bin_output)
+        return nt_rep, bin_alpha
+
+class att_chrome(nn.Module):
+    def __init__(self, args):
+        super(att_chrome, self).__init__()
+        self.n_nts = args.n_nts
+        self.n_bins = args.n_bins
+        self.ip_bin_size = 1
+
+        self.encoder = recurrent_encoder(self.n_bins, self.n_nts, False, args)
+        self.opsize = self.encoder.outputlength()
+        self.linear = nn.Linear(self.opsize, 2)
+
+    def forward(self, iput):
+
+        [batch_size, _, _] = iput.size()
+        level1_rep, bin_a = self.encoder(iput)
+        level1_rep = level1_rep.squeeze(1)
+        bin_pred = self.linear(level1_rep)
+        sigmoid_pred = torch.sigmoid(bin_pred)
+        return sigmoid_pred, bin_a
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
